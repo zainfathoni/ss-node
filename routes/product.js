a00d46adc0270e077ee3fce242a1e3e14a001ba3
Zain Fathoni <zain.fathoni@gmail.com>
2016-04-03 21:27:32 +0700

    [Add] Product Add

--- routes/product.js
+++ routes/product.js
@@ -1,10 +1,43 @@
-// ROUTES
-// Get an instance of router
 var router = require('express').Router();
+var mongo = require('mongodb');
+var model = require('../model')
+
+var MongoClient = mongo.MongoClient;
+var ObjectID = mongo.ObjectID;
+var url = 'mongodb://localhost:27017/ss-node';
+var table = 'product';
+
+// Parameter 'id' Validation
+router.param('id', function(req, res, next, id) {
+    if (!ObjectID.isValid(id)) {
+        next(new Error('ObjectID is invalid.'));
+    } else {
+        req.id = ObjectID.createFromHexString(id);
+        next();
+    }
+});
 
 // Add
-router.post('/', function(req, res) {
-    res.send('Add');
+router.post('/', function(req, res, next) {
+    var item = req.body;
+    
+    MongoClient.connect(url, function(err, db) {
+        if (err) return next(err);
+        var coll = db.collection(table);
+    
+        // If similar Name found, update the content
+        model.findByName(coll, item.name, next, function(result) {
+            if (result) { // Update
+                model.update(coll, result._id, item, next, function(result) {
+                    res.send(result);
+                });
+            } else { // Insert
+                model.insert(coll, item, next, function(result) {
+                    res.send(result);
+                });
+            }
+        });
+    });
 });
 
 // Find All
