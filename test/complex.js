f4e830e1db88403e11d8aa4471240776f9437d1f
Zain Fathoni <zain.fathoni@gmail.com>
2016-04-04 16:32:38 +0700

    [Modify] Put spacing between subtest

--- test/complex.js
+++ test/complex.js
@@ -44,7 +44,7 @@ describe('Complex Logic Test', function() {
             });
     });
     
-    it('Delete ABC Product', function(done) {
+    it('Delete ABC Product\n', function(done) {
         server
             .delete('/product/name/ABC')
             .expect('Content-type', /json/)
@@ -78,7 +78,7 @@ describe('Complex Logic Test', function() {
             });
     });
     
-    it('Find A Category, parent should be null', function(done) {
+    it('Find A Category, parent should be null\n', function(done) {
         server
             .get('/category/name/A')
             .expect('Content-type', /json/)
@@ -113,7 +113,7 @@ describe('Complex Logic Test', function() {
             });
     });
     
-    it('Find ABC Product, parent should be A', function(done) {
+    it('Find ABC Product, parent should be A\n', function(done) {
         server
             .get('/product/name/ABC')
             .expect('Content-type', /json/)
@@ -148,7 +148,7 @@ describe('Complex Logic Test', function() {
             });
     });
     
-    it('Find X Category, parent should be A', function(done) {
+    it('Find X Category, parent should be A\n', function(done) {
         server
             .get('/category/name/X')
             .expect('Content-type', /json/)
@@ -167,6 +167,21 @@ describe('Complex Logic Test', function() {
     it('Delete ABC Product', function(done) {
         server
             .delete('/product/name/ABC')
+            .expect('Content-type', /json/)
+            .expect(200)
+            .end(function(err, res) {
+                assert.equal(res.status, 200);      // HTTP status 200
+                assert.equal(res.body.error, undefined);    // No Error
+                // Delete Successful
+                assert.equal(res.body.ok, 1);
+                assert.equal(res.body.n, 1);
+                done();
+            });
+    });
+    
+    it('Delete X Category', function(done) {
+        server
+            .delete('/category/name/X')
             .expect('Content-type', /json/)
             .expect(200)
             .end(function(err, res) {
