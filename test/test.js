6309e374674e0f6139a7b357cc6124fe4b389d3a
Zain Fathoni <zain.fathoni@gmail.com>
2016-04-04 11:32:58 +0700

    [Add] Full Unit Test for Product

--- test/test.js
+++ test/test.js
@@ -7,24 +7,20 @@ var server = supertest.agent('http://localhost:' + port);
 
 // Product Unit Test
 describe('Product Unit Test', function() {
-    
-    // #1 Find All Products
-    it('should return all products', function(done) {
+
+    it('Find All Products', function(done) {
         server
             .get('/product')
             .expect('Content-type', /json/)
             .expect(200)
             .end(function(err, res) {
-                // HTTP status should be 200
-                assert.equal(res.status, 200);
-                // Error key should be false.
-                assert.equal(res.body.error, undefined);
+                assert.equal(res.status, 200);      // HTTP status 200
+                assert.equal(res.body.error, undefined);    // No Error
                 done();
             });
     });
 
-    // #2 Insert Tuxedo Product
-    it('should insert tuxedo product', function(done) {
+    it('Insert Tuxedo Product', function(done) {
         server
             .post('/product')
             .send({
@@ -35,15 +31,65 @@ describe('Product Unit Test', function() {
             .expect('Content-type', /json/)
             .expect(200)
             .end(function(err, res) {
-                // HTTP status should be 200
-                assert.equal(res.status, 200);
-                // Error key should be false.
-                assert.equal(res.body.error, undefined);
-                assert.equal(res.body.ok, 0);
-                assert.equal(res.body.n, 0);
+                assert.equal(res.status, 200);      // HTTP status 200
+                assert.equal(res.body.error, undefined);    // No Error
+                // Insert Successful
+                assert.equal(res.body.ok, 1);
+                assert.equal(res.body.n, 1);
+                done();
+            });
+    });
+    
+    it('Update Tuxedo Product', function(done) {
+        server
+            .put('/product/name/Tuxedo')
+            .send({
+                "name": "Tuxedo",
+                "price": 4050000,
+                "parent": "Top Wear"
+            })
+            .expect('Content-type', /json/)
+            .expect(200)
+            .end(function(err, res) {
+                assert.equal(res.status, 200);      // HTTP status 200
+                assert.equal(res.body.error, undefined);    // No Error
+                // Update Successful
+                assert.equal(res.body.ok, 1);
+                assert.equal(res.body.nModified, 1);
+                assert.equal(res.body.n, 1);
                 done();
             });
     });
 
+    it('Find Tuxedo Product', function(done) {
+        server
+            .get('/product/name/Tuxedo')
+            .expect('Content-type', /json/)
+            .expect(200)
+            .end(function(err, res) {
+                assert.equal(res.status, 200);      // HTTP status 200
+                assert.equal(res.body.error, undefined);    // No Error
+                // Insert Successful
+                assert.equal(res.body.name, "Tuxedo");
+                assert.equal(res.body.price, 4050000);
+                assert.equal(res.body.parent, "Top Wear");
+                done();
+            });
+    });
+    
+    it('Delete Tuxedo Product', function(done) {
+        server
+            .delete('/product/name/Tuxedo')
+            .expect('Content-type', /json/)
+            .expect(200)
+            .end(function(err, res) {
+                assert.equal(res.status, 200);      // HTTP status 200
+                assert.equal(res.body.error, undefined);    // No Error
+                // Delete Successful
+                assert.equal(res.body.ok, 1);
+                assert.equal(res.body.n, 1);
+                done();
+            });
+    });
 
 });
